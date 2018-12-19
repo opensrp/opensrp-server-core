a8719fdcbdb65850594724e7ea620208a11b8366
Manu Tarus <manutarus@gmail.com>
2018-12-19 14:40:16 +0300

    Update task service test

--- src/test/java/org/opensrp/service/TaskServiceTest.java
+++ src/test/java/org/opensrp/service/TaskServiceTest.java
@@ -189,6 +189,7 @@ public class TaskServiceTest {
 
 		ArgumentCaptor<Task> argumentCaptor = ArgumentCaptor.forClass(Task.class);
 		verify(taskRepository).update(argumentCaptor.capture());
+		assertEquals(task.getServerVersion().longValue(),argumentCaptor.getValue().getServerVersion().longValue());
 		assertEquals("Not Sprayable",argumentCaptor.getValue().getBusinessStatus());
 
 		assertEquals("tsk11231jh22", task.getIdentifier());
