27db10815e2f883a974af9121d3e0c30eb0f11e3
Samuel Githengi <samuel.githengi@gmail.com>
2019-04-30 20:13:25 +0300

    Implement scripts for getting structures within radius

--- src/main/java/org/opensrp/repository/postgres/mapper/custom/CustomStructureMapper.java
+++ src/main/java/org/opensrp/repository/postgres/mapper/custom/CustomStructureMapper.java
@@ -1,8 +1,13 @@
 package org.opensrp.repository.postgres.mapper.custom;
 
+import java.util.List;
+
+import org.opensrp.domain.LocationProperty;
 import org.opensrp.domain.postgres.Structure;
 import org.opensrp.repository.postgres.mapper.StructureMapper;
 
 public interface CustomStructureMapper extends StructureMapper {
 	int insertSelectiveAndSetId(Structure structure);
+
+	List<LocationProperty> selectWithinRadius(double latitude, double longitude, double radius);
 }
