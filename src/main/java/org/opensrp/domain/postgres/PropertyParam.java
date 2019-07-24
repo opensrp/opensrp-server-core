31924e044de544ce721ddcb1ab8eb1e8ebd6a076
Samuel Githengi <samuel.githengi@gmail.com>
2019-07-24 19:55:34 +0300

    Add function for getting locations by properties

---
+++ src/main/java/org/opensrp/domain/postgres/PropertyParam.java
@@ -0,0 +1,24 @@
+package org.opensrp.domain.postgres;
+
+public class PropertyParam {
+	public String paramName;
+
+	public String paramValue;
+
+	public String getParamName() {
+		return paramName;
+	}
+
+	public void setParamName(String paramName) {
+		this.paramName = paramName;
+	}
+
+	public String getParamValue() {
+		return paramValue;
+	}
+
+	public void setParamValue(String paramValue) {
+		this.paramValue = paramValue;
+	}
+
+}
--- src/main/java/org/opensrp/repository/postgres/mapper/custom/CustomLocationMetadataMapper.java
+++ src/main/java/org/opensrp/repository/postgres/mapper/custom/CustomLocationMetadataMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.opensrp.domain.postgres.Location;
 import org.opensrp.domain.postgres.LocationMetadataExample;
+import org.opensrp.domain.postgres.PropertyParam;
 import org.opensrp.repository.postgres.mapper.LocationMetadataMapper;
 
 public interface CustomLocationMetadataMapper extends LocationMetadataMapper {
@@ -12,6 +13,11 @@ public interface CustomLocationMetadataMapper extends LocationMetadataMapper {
 	Location findById(String id);
 
 	List<Location> selectMany(@Param("example") LocationMetadataExample locationMetadataExample,
+			@Param("offset") int offset, @Param("limit") int limit);
+	
+	
+	List<Location> selectManyByProperties(@Param("example") LocationMetadataExample locationMetadataExample,
+			@Param("properties") List<PropertyParam> properties,
 			@Param("offset") int offset, @Param("limit") int limit);
 
 }
