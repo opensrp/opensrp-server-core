b91e5179e00a5b4dab678da577393451c9024646
Ephraim Kigamba <ekigamba@ona.io>
2019-08-16 14:28:22 +0300

    Add JSONBTypeHandler class to handle jsonb fields in client form and client form metadata

--- src/main/java/org/opensrp/repository/postgres/handler/JSONBTypeHandler.java
+++ src/main/java/org/opensrp/repository/postgres/handler/JSONBTypeHandler.java
@@ -1,48 +1,48 @@
 package org.opensrp.repository.postgres.handler;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
-import org.apache.ibatis.type.TypeHandler;
-import org.opensrp.domain.Client;
-import org.opensrp.domain.ClientForm;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.postgresql.util.PGobject;
 
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ClientFormTypeHandler extends BaseTypeHandler implements TypeHandler<ClientForm> {
+public class JSONBTypeHandler extends BaseTypeHandler<Object>{
+
+	private static final PGobject jsonObject = new PGobject();
+
 	@Override
-	public void setParameter(PreparedStatement preparedStatement, int i, ClientForm clientForm, JdbcType jdbcType) throws SQLException {
-		/*try {
-			if (clientForm != null) {
-				String jsonString = mapper.writeValueAsString(clientForm);
-				PGobject jsonObject = new PGobject();
-				jsonObject.setType("jsonb");
-				jsonObject.setValue(jsonString);
-				preparedStatement.setObject(i, jsonObject);
-			}
+	public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+		jsonObject.setType("json");
+
+		try {
+			jsonObject.setValue(new ObjectMapper().writeValueAsString(parameter));  //Converting java objects to json strings
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		catch (Exception e) {
-			throw new SQLException(e);
-		}*/
+
+		ps.setObject(i, jsonObject);
 	}
 
 	@Override
-	public ClientForm getResult(ResultSet resultSet, String s) throws SQLException {
-		return null;
+	public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		return rs.getString(columnName);                                 // Return to String
 	}
 
 	@Override
-	public ClientForm getResult(ResultSet resultSet, int i) throws SQLException {
-		return null;
+	public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		return rs.getString(columnIndex);
 	}
 
 	@Override
-	public ClientForm getResult(CallableStatement callableStatement, int i) throws SQLException {
-		return null;
+	public Object getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		return cs.getString(columnIndex);
 	}
+
 	
 	/*@Override
 	public void setParameter(PreparedStatement ps, int i, Client parameter, JdbcType jdbcType) throws SQLException {
