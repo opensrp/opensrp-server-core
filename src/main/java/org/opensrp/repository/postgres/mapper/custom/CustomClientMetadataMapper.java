e5c8debaf4df47ed44ac98119444cc1f1b608c74
proshanto <proshanto123@gmail.com>
2019-09-02 19:18:55 +0600

    fetch household head, member count,provider and total count

--- src/main/java/org/opensrp/repository/postgres/mapper/custom/CustomClientMetadataMapper.java
+++ src/main/java/org/opensrp/repository/postgres/mapper/custom/CustomClientMetadataMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.opensrp.domain.postgres.Client;
 import org.opensrp.domain.postgres.ClientMetadataExample;
+import org.opensrp.domain.postgres.HouseholdClient;
 import org.opensrp.repository.postgres.mapper.ClientMetadataMapper;
 import org.opensrp.search.AddressSearchBean;
 import org.opensrp.search.ClientSearchBean;
@@ -12,15 +13,21 @@ import org.opensrp.search.ClientSearchBean;
 public interface CustomClientMetadataMapper extends ClientMetadataMapper {
 	
 	List<Client> selectMany(@Param("example") ClientMetadataExample example, @Param("offset") int offset,
-	        @Param("limit") int limit);
+	                        @Param("limit") int limit);
 	
 	Client selectOne(String baseEntityId);
 	
 	Client selectByDocumentId(String documentId);
 	
 	List<Client> selectBySearchBean(@Param("clientBean") ClientSearchBean searchBean,
-	        @Param("addressBean") AddressSearchBean addressSearchBean, @Param("offset") int offset,
-	        @Param("limit") int limit);
+	                                @Param("addressBean") AddressSearchBean addressSearchBean, @Param("offset") int offset,
+	                                @Param("limit") int limit);
+	
+	HouseholdClient selectCountBySearchBean(@Param("clientBean") ClientSearchBean searchBean,
+	                                        @Param("addressBean") AddressSearchBean addressSearchBean);
 	
 	List<Client> selectByName(@Param("name") String nameMatches, @Param("offset") int offset, @Param("limit") int limit);
+	
+	List<HouseholdClient> selectMemberCountHouseholdHeadProviderByClients(@Param("example") ClientMetadataExample example,
+	                                                                      @Param("clientType") String clientType);
 }
