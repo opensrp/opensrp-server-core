f028039c1a3e98ab8b5d077c8601354d2feb4585
proshanto <proshanto123@gmail.com>
2019-10-24 13:04:13 +0600

    add custom client class

---
+++ src/main/java/org/opensrp/domain/postgres/CustomClient.java
@@ -0,0 +1,63 @@
+package org.opensrp.domain.postgres;
+
+import java.util.Date;
+
+public class CustomClient extends Client {
+	
+	private Date lastContactDate;
+	
+	private Date edd;
+	
+	private String gestationalAge;
+	
+	private String riskCategory;
+	
+	private String immunizationStatus;
+	
+	public Date getLastContactDate() {
+		return lastContactDate;
+	}
+	
+	public void setLastContactDate(Date lastContactDate) {
+		this.lastContactDate = lastContactDate;
+	}
+	
+	public Date getEdd() {
+		return edd;
+	}
+	
+	public void setEdd(Date edd) {
+		this.edd = edd;
+	}
+	
+	public String getGestationalAge() {
+		return gestationalAge;
+	}
+	
+	public void setGestationalAge(String gestationalAge) {
+		this.gestationalAge = gestationalAge;
+	}
+	
+	public String getRiskCategory() {
+		return riskCategory;
+	}
+	
+	public void setRiskCategory(String riskCategory) {
+		this.riskCategory = riskCategory;
+	}
+	
+	public String getImmunizationStatus() {
+		return immunizationStatus;
+	}
+	
+	public void setImmunizationStatus(String immunizationStatus) {
+		this.immunizationStatus = immunizationStatus;
+	}
+	
+	@Override
+	public String toString() {
+		return "CustomClient [lastContactDate=" + lastContactDate + ", edd=" + edd + ", gestationalAge=" + gestationalAge
+		        + ", riskCategory=" + riskCategory + ", immunizationStatus=" + immunizationStatus + "]";
+	}
+	
+}
