1bf6593762975e95281a0246e0b60e8e0b82d08d
Vincent Karuri <vincentmkaruri@gmail.com>
2020-01-09 16:58:31 +0300

    Remove abstract modifier from base spring context provider

--- src/test/java/org/opensrp/SpringApplicationContextProvider.java
+++ src/test/java/org/opensrp/SpringApplicationContextProvider.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-applicationContext-opensrp.xml")
-public abstract class SpringApplicationContextProvider extends TestResourceLoader {
+public class SpringApplicationContextProvider extends TestResourceLoader {
 	
 	public static ApplicationContext context;
 	
