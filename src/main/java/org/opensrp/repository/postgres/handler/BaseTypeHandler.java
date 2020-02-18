87c3129844fa67c782460e3860c77612dc1e5d4e
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-18 19:21:58 +0300

    Upgrade serialization to fasterxml jackson i.e json 2.0

--- src/main/java/org/opensrp/repository/postgres/handler/BaseTypeHandler.java
+++ src/main/java/org/opensrp/repository/postgres/handler/BaseTypeHandler.java
@@ -1,18 +1,18 @@
 package org.opensrp.repository.postgres.handler;
 
-import org.codehaus.jackson.Version;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.module.SimpleModule;
 import org.ektorp.impl.StdObjectMapperFactory;
 import org.joda.time.DateTime;
 import org.opensrp.util.DateTimeDeserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class BaseTypeHandler {
 	
 	public static final ObjectMapper mapper = new StdObjectMapperFactory().createObjectMapper();;
 	
 	protected BaseTypeHandler() {
-		SimpleModule dateTimeModule = new SimpleModule("DateTimeModule", new Version(0, 0, 0, null));
+		SimpleModule dateTimeModule = new SimpleModule("DateTimeModule");
 		dateTimeModule.addDeserializer(DateTime.class, new DateTimeDeserializer());
 		mapper.registerModule(dateTimeModule);
 	}
--- src/main/java/org/opensrp/util/DateTimeDeserializer.java
+++ src/main/java/org/opensrp/util/DateTimeDeserializer.java
@@ -2,28 +2,30 @@ package org.opensrp.util;
 
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonToken;
-import org.codehaus.jackson.map.DeserializationContext;
-import org.codehaus.jackson.map.deser.std.StdScalarDeserializer;
 import org.joda.time.DateTime;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+
 public class DateTimeDeserializer extends StdScalarDeserializer<DateTime> {
+	
+	private static final long serialVersionUID = -805075518081134882L;
 	
 	public DateTimeDeserializer() {
 		super(DateTime.class);
 	}
 	
 	@Override
-	public DateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-	        throws JsonParseException, IOException {
+	public DateTime deserialize(JsonParser jsonParser, DeserializationContext ctxt)
+	        throws IOException, JsonProcessingException {
 		JsonToken currentToken = jsonParser.getCurrentToken();
 		if (currentToken == JsonToken.VALUE_STRING) {
 			String dateTimeAsString = jsonParser.getText().trim();
 			return new DateTime(dateTimeAsString);
 		}
 		return null;
-		
 	}
 }
