514e11d24dcc7c56cf38b71b73ce2c6d0401baef
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-20 10:50:42 +0300

    configure datetime serializer

--- src/main/java/org/opensrp/repository/postgres/handler/BaseTypeHandler.java
+++ src/main/java/org/opensrp/repository/postgres/handler/BaseTypeHandler.java
@@ -3,6 +3,7 @@ package org.opensrp.repository.postgres.handler;
 import org.ektorp.impl.StdObjectMapperFactory;
 import org.joda.time.DateTime;
 import org.opensrp.util.DateTimeDeserializer;
+import org.opensrp.util.DateTimeSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -14,6 +15,7 @@ public class BaseTypeHandler {
 	protected BaseTypeHandler() {
 		SimpleModule dateTimeModule = new SimpleModule("DateTimeModule");
 		dateTimeModule.addDeserializer(DateTime.class, new DateTimeDeserializer());
+		dateTimeModule.addSerializer(DateTime.class, new DateTimeSerializer());
 		mapper.registerModule(dateTimeModule);
 	}
 	
---
+++ src/main/java/org/opensrp/util/DateTimeSerializer.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.opensrp.util;
+
+import java.io.IOException;
+
+import org.joda.time.DateTime;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
+
+/**
+ * @author Samuel Githengi created on 02/20/20
+ */
+public class DateTimeSerializer extends StdScalarSerializer<DateTime> {
+	
+	private static final long serialVersionUID = -6797135374503717200L;
+	
+	public DateTimeSerializer() {
+		super(DateTime.class);
+	}
+	
+	@Override
+	public void serialize(DateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+		gen.writeString(value.toString());
+	}
+	
+}
