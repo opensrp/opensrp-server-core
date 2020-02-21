fa2159b86e893ac9f4e79869832bc4f1df143a39
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-21 14:39:58 +0300

    Implement dummy Ihandler

--- src/main/java/org/opensrp/service/formSubmission/handler/IHandlerMapper.java
+++ src/main/java/org/opensrp/service/formSubmission/handler/IHandlerMapper.java
@@ -1,10 +1,18 @@
 package org.opensrp.service.formSubmission.handler;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public interface IHandlerMapper {
+import org.springframework.stereotype.Component;
+
+@Component
+public class IHandlerMapper {
 	
-	public Map<String, EventsHandler> handlerMap();
+	public Map<String, EventsHandler> handlerMap() {
+		return new HashMap<>();
+	}
 	
-	public Map<String, EventsHandler> addHandler(String name, EventsHandler handler);
+	public Map<String, EventsHandler> addHandler(String name, EventsHandler handler) {
+		return new HashMap<>();
+	}
 }
