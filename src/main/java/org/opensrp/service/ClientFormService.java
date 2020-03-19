c291da51896e1c8c134abdfe10043982f18822fc
Ephraim Kigamba <ekigamba@ona.io>
2020-03-19 15:29:39 +0300

    Add addClientForm method in ClientService
    - Enable adding a ClientForm & ClientFormMetadata
    - Fix the ClientFormMapper using client_forms as table name instead of client_form

--- src/main/java/org/opensrp/service/ClientFormService.java
+++ src/main/java/org/opensrp/service/ClientFormService.java
@@ -1,11 +1,15 @@
 package org.opensrp.service;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import org.opensrp.domain.IdVersionTuple;
 import org.opensrp.domain.postgres.ClientForm;
 import org.opensrp.domain.postgres.ClientFormMetadata;
 import org.opensrp.repository.postgres.mapper.ClientFormMapper;
+import org.opensrp.repository.postgres.mapper.custom.CustomClientFormMapper;
 import org.opensrp.repository.postgres.mapper.custom.CustomClientFormMetadataMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +18,14 @@ import java.util.List;
 @Service
 public class ClientFormService {
 
-    private final ClientFormMapper clientFormMapper;
-
+    private final CustomClientFormMapper clientFormMapper;
     private final CustomClientFormMetadataMapper clientFormMetadataMapper;
 
+    private static Logger logger = LoggerFactory.getLogger(ClientFormService.class.toString());
+
+
     @Autowired
-    public ClientFormService(ClientFormMapper clientFormMapper, CustomClientFormMetadataMapper clientFormMetadataMapper) {
+    public ClientFormService(CustomClientFormMapper clientFormMapper, CustomClientFormMetadataMapper clientFormMetadataMapper) {
         this.clientFormMapper = clientFormMapper;
         this.clientFormMetadataMapper = clientFormMetadataMapper;
     }
@@ -44,4 +50,49 @@ public class ClientFormService {
     public ClientFormMetadata getClientFormMetadataById(int formId) {
         return clientFormMetadataMapper.selectByPrimaryKey(formId);
     }
+
+    @Nullable
+    public CompleteClientForm addClientForm(@NonNull ClientForm clientForm, @NonNull ClientFormMetadata clientFormMetadata) {
+        Integer clientFormId = clientForm.getId();
+        Integer clientFormMetadataId = clientFormMetadata.getId();
+        if ((clientFormId != null && clientFormId != 0) || (clientFormMetadataId != null && clientFormMetadataId != 0)) {
+            logger.error("ClientForm & ClientFormMetadata Id must be NULL or 0", new Exception());
+            return null;
+        }
+
+        // Check if the same client form with that version & identifier exists
+        ClientFormMetadata clientFormMetadataResult = clientFormMetadataMapper.selectClientFormMetadataByFormVersionAndIdentifier(clientFormMetadata.getVersion(), clientFormMetadata.getIdentifier());
+        if (clientFormMetadataResult != null) {
+            logger.error("ClientFormMetadata with version " + clientFormMetadata.getVersion() + " and Identifier "
+                    + clientFormMetadata.getIdentifier() + " Already exists", new Exception());
+            return null;
+        }
+
+        int rowsAffected = clientFormMapper.insertClientForm(clientForm);
+        if (rowsAffected > 0) {
+            logger.info("Generated id for Client form is " + clientForm.getId());
+
+            clientFormMetadata.setId(clientForm.getId());
+            int resultClientFormMetadataId = clientFormMetadataMapper.insertClientFormMetadata(clientFormMetadata);
+            logger.info("Generated id for Client Form Metadata is " + resultClientFormMetadataId);
+
+            return new CompleteClientForm(clientForm, clientFormMetadata);
+        }
+
+        return null;
+    }
+
+    public static class CompleteClientForm {
+
+        public ClientForm clientForm;
+        public ClientFormMetadata clientFormMetadata;
+
+        public CompleteClientForm(@NonNull ClientForm clientForm, @NonNull ClientFormMetadata clientFormMetadata) {
+            this.clientForm = clientForm;
+            this.clientFormMetadata = clientFormMetadata;
+        }
+    }
+
+
+
 }
