f03db01ba69fd4fabde2394c1d1d4a4498773c99
Ephraim Kigamba <ekigamba@ona.io>
2020-06-15 09:57:17 +0300

    Code cleanup

--- src/test/java/org/opensrp/service/ClientFormServiceTest.java
+++ src/test/java/org/opensrp/service/ClientFormServiceTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class ClientFormServiceTest extends BaseRepositoryTest {
 
