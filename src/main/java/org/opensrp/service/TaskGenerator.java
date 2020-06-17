7f89f028047952a200511cbc097170a89e907603
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-17 18:31:59 +0300

    active task generation

--- src/main/java/org/opensrp/service/TaskGenerator.java
+++ src/main/java/org/opensrp/service/TaskGenerator.java
@@ -8,18 +8,20 @@ import org.smartregister.pathevaluator.plan.PlanEvaluator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-
 /**
  * @author Samuel Githengi created on 06/12/20
  */
 @Service
 public class TaskGenerator {
 	
+	private PlanEvaluator planEvaluator;
+	
+	public TaskGenerator() {
+		planEvaluator = new PlanEvaluator();
+	}
+	
 	@Async
 	public void processPlanEvaluation(PlanDefinition planDefinition, PlanDefinition existingPlanDefinition) {
-		//TODO consider moving PlanEvaluator to container managed bean
-		PlanEvaluator planEvaluator = new PlanEvaluator();
-		//TODO uncomment after domains are moved to plan evaluator lib
-		//planEvaluator.evaluatePlan(planDefinition, existingPlanDefinition);
+		planEvaluator.evaluatePlan(planDefinition, existingPlanDefinition);
 	}
 }
