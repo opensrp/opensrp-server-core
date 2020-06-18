70f3246cea8af453985967df51eeb61ac9bd0bf1
Allan O <a.onchuru@gmail.com>
2020-06-18 19:33:06 +0300

    :recycle: Fix Codacy reported issue with assert imports

--- src/test/java/org/opensrp/service/ClientFormServiceTest.java
+++ src/test/java/org/opensrp/service/ClientFormServiceTest.java
@@ -11,7 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class ClientFormServiceTest extends BaseRepositoryTest {
 
