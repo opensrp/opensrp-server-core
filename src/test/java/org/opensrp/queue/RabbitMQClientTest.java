c9c591bbd9c7c8d4ac94d689eecfe287e6d534eb
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-08-04 16:00:57 +0500

    286 : Add unit test for RabbitTemplate testing

---
+++ src/test/java/org/opensrp/queue/RabbitMQClientTest.java
@@ -0,0 +1,76 @@
+package org.opensrp.queue;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opensrp.config.RabbitMQConfig;
+import org.smartregister.domain.Jurisdiction;
+import org.smartregister.pathevaluator.TriggerType;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-applicationContext-opensrp.xml")
+public class RabbitMQClientTest {
+
+	@Autowired
+	private AmqpAdmin amqpAdmin;
+
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+
+	@Autowired
+	private RabbitMQSender rabbitMQSender;
+
+	@Autowired
+	private RabbitMQReceiver rabbitMQReceiver;
+
+	@Autowired
+	private Queue queue;
+
+	@Configuration
+	@Import(RabbitMQConfig.class) // the actual configuration
+	public static class TestConfig {
+
+		@Bean
+		MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+			SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+			simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+			return simpleMessageListenerContainer;
+
+		}
+	}
+
+	@Test
+	public void simpleProducerConsumerTest() {
+		try {
+			CustomPlanEvaluatorMessage customPlanEvaluatorMessage = new CustomPlanEvaluatorMessage();
+			Jurisdiction jurisdiction = new Jurisdiction();
+			jurisdiction.setCode("test-loc1");
+			customPlanEvaluatorMessage.setJurisdiction(jurisdiction);
+			customPlanEvaluatorMessage.setTriggerType(TriggerType.PLAN_ACTIVATION);
+			amqpAdmin.declareQueue(new Queue(queue.getName(), false));
+
+			rabbitTemplate.convertAndSend("javainuse.exchange", "javainuse.routingkey", customPlanEvaluatorMessage);
+			CustomPlanEvaluatorMessage received = (CustomPlanEvaluatorMessage) rabbitTemplate.receiveAndConvert();
+			assertEquals(customPlanEvaluatorMessage.getJurisdiction().getCode(), received.getJurisdiction().getCode());
+			assertEquals(customPlanEvaluatorMessage.getTriggerType().name(), received.getTriggerType().name());
+		}
+		catch (AmqpException e) {
+			Assert.fail("Test failed: " + e.getLocalizedMessage());
+		}
+	}
+}
