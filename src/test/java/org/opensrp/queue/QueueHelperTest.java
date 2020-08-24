3e96bda2184438da20f74e887c4084914f6e84b0
Reham Muzzamil <reham.muzzamil@venturedive.com>
2020-08-24 20:23:14 +0500

    Increase test coverage

--- src/test/java/org/opensrp/queue/QueueHelperTest.java
+++ src/test/java/org/opensrp/queue/QueueHelperTest.java
@@ -21,6 +21,7 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.ArgumentCaptor;
+import org.opensrp.repository.PlanRepository;
 import org.opensrp.service.PlanService;
 import org.powermock.reflect.Whitebox;
 import org.smartregister.domain.Action;
@@ -52,6 +53,7 @@ import static org.mockito.Matchers.nullable;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.never;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-applicationContext-opensrp.xml")
@@ -78,6 +80,9 @@ public class QueueHelperTest {
 	@Autowired
 	private AmqpAdmin amqpAdmin;
 
+	@Autowired
+	private PlanRepository planRepository;
+
 	@Captor
 	private ArgumentCaptor<Jurisdiction> argumentCaptor = ArgumentCaptor.forClass(Jurisdiction.class);
 
@@ -85,8 +90,6 @@ public class QueueHelperTest {
 
 	public static Gson gson = new GsonBuilder().registerTypeAdapter(DateTime.class, new TaskDateTimeTypeConverter())
 			.registerTypeAdapter(LocalDate.class, new DateTypeConverter()).create();
-
-	public static String plan = "{\"identifier\":\"d18f15ec-afaf-42f3-ba96-d207c456645b\",\"version\":\"2\",\"name\":\"A1-59ad4fa0-1945-4b50-a6e3-a056a7cdceb2-2019-09-09\",\"title\":\"A1 - Ban Khane Chu OA - 2019-09-09(deprecated)\",\"status\":\"retired\",\"date\":\"2019-09-09\",\"effectivePeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"useContext\":[{\"code\":\"interventionType\",\"valueCodableConcept\":\"FI\"},{\"code\":\"fiStatus\",\"valueCodableConcept\":\"A1\"},{\"code\":\"fiReason\",\"valueCodableConcept\":\"Routine\"},{\"code\":\"caseNum\",\"valueCodableConcept\":\"3336\"},{\"code\":\"opensrpEventId\",\"valueCodableConcept\":\"75049cd6-d77b-4239-9092-7bd1aa0d438c\"},{\"code\":\"taskGenerationStatus\",\"valueCodableConcept\":\"True\"}],\"jurisdiction\":[{\"code\":\"59ad4fa0-1945-4b50-a6e3-a056a7cdceb2\"}],\"serverVersion\":1568110931837,\"goal\":[{\"id\":\"Case_Confirmation\",\"description\":\"Confirm the index case\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Number of cases confirmed\",\"detail\":{\"detailQuantity\":{\"value\":1,\"comparator\":\">=\",\"unit\":\"case(s)\"}},\"due\":\"2019-09-19\"}]},{\"id\":\"RACD_register_families\",\"description\":\"Register all families and family members in all residential structures enumerated or added (100%) within operational area\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Percent of residential structures with full family registration\",\"detail\":{\"detailQuantity\":{\"value\":100,\"comparator\":\">=\",\"unit\":\"Percent\"}},\"due\":\"2019-09-29\"}]},{\"id\":\"RACD_Blood_Screening\",\"description\":\"Visit all residential structures (100%) within a 1 km radius of a confirmed index case and test each registered person\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Number of registered people tested\",\"detail\":{\"detailQuantity\":{\"value\":50,\"comparator\":\">=\",\"unit\":\"Person(s)\"}},\"due\":\"2019-09-29\"}]},{\"id\":\"RACD_bednet_distribution\",\"description\":\"Visit 100% of residential structures in the operational area and provide nets\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Percent of residential structures received nets\",\"detail\":{\"detailQuantity\":{\"value\":90,\"comparator\":\">=\",\"unit\":\"Percent\"}},\"due\":\"2019-09-29\"}]},{\"id\":\"Larval_Dipping\",\"description\":\"Perform a minimum of three larval dipping activities in the operational area\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Number of larval dipping activities completed\",\"detail\":{\"detailQuantity\":{\"value\":3,\"comparator\":\">=\",\"unit\":\"activit(y|ies)\"}},\"due\":\"2019-09-29\"}]},{\"id\":\"Mosquito_Collection\",\"description\":\"Set a minimum of three mosquito collection traps and complete the mosquito collection process\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Number of mosquito collection activities completed\",\"detail\":{\"detailQuantity\":{\"value\":3,\"comparator\":\">=\",\"unit\":\"activit(y|ies)\"}},\"due\":\"2019-09-29\"}]},{\"id\":\"BCC_Focus\",\"description\":\"Complete at least 1 BCC activity for the operational area\",\"priority\":\"medium-priority\",\"target\":[{\"measure\":\"Number of BCC Activities Completed\",\"detail\":{\"detailQuantity\":{\"value\":1,\"comparator\":\">=\",\"unit\":\"activit(y|ies)\"}},\"due\":\"2019-09-29\"}]}],\"action\":[{\"identifier\":\"baebfd98-2f1f-4a5e-9478-680f13b45697\",\"prefix\":1,\"title\":\"Case Confirmation\",\"description\":\"Confirm the index case\",\"code\":\"Case Confirmation\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-19\"},\"reason\":\"Investigation\",\"goalId\":\"Case_Confirmation\",\"subjectCodableConcept\":{\"text\":\"Case_Confirmation\"},\"taskTemplate\":\"Case_Confirmation\"},{\"identifier\":\"9f554b5a-0d00-4f18-996f-30dce50e57d8\",\"prefix\":2,\"title\":\"Family Registration\",\"description\":\"Register all families & family members in all residential structures enumerated (100%) within the operational area\",\"code\":\"RACD Register Family\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"reason\":\"Investigation\",\"goalId\":\"RACD_register_families\",\"subjectCodableConcept\":{\"text\":\"Residential_Structure\"},\"taskTemplate\":\"RACD_register_families\"},{\"identifier\":\"28164940-e2cb-4622-8153-b6ac8ff6e940\",\"prefix\":3,\"title\":\"Blood screening\",\"description\":\"Visit all residential structures (100%) within a 1 km radius of a confirmed index case and test each registered person\",\"code\":\"Blood Screening\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"reason\":\"Investigation\",\"goalId\":\"RACD_Blood_Screening\",\"subjectCodableConcept\":{\"text\":\"Person\"},\"taskTemplate\":\"RACD_Blood_Screening\"},{\"identifier\":\"b9f68512-376f-49c9-bb53-80d9e81a73d2\",\"prefix\":4,\"title\":\"Bednet Distribution\",\"description\":\"Visit 100% of residential structures in the operational area and provide nets\",\"code\":\"Bednet Distribution\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"reason\":\"Routine\",\"goalId\":\"RACD_bednet_distribution\",\"subjectCodableConcept\":{\"text\":\"Residential_Structure\"},\"taskTemplate\":\"Bednet_Distribution\"},{\"identifier\":\"3b3f265d-05fe-4c16-a5a1-141bac82f128\",\"prefix\":5,\"title\":\"Larval Dipping\",\"description\":\"Perform a minimum of three larval dipping activities in the operational area\",\"code\":\"Larval Dipping\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"reason\":\"Investigation\",\"goalId\":\"Larval_Dipping\",\"subjectCodableConcept\":{\"text\":\"Breeding_Site\"},\"taskTemplate\":\"Larval_Dipping\"},{\"identifier\":\"c295655a-8630-4bc1-a5fd-79b22a9b1125\",\"prefix\":6,\"title\":\"Mosquito Collection\",\"description\":\"Set a minimum of three mosquito collection traps and complete the mosquito collection process\",\"code\":\"Mosquito Collection\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"reason\":\"Investigation\",\"goalId\":\"Mosquito_Collection\",\"subjectCodableConcept\":{\"text\":\"Mosquito_Collection_Point\"},\"taskTemplate\":\"Mosquito_Collection_Point\"},{\"identifier\":\"8b899c73-56c8-40a2-a738-6a62b459e8f8\",\"prefix\":7,\"title\":\"Behaviour Change Communication\",\"description\":\"Conduct BCC activity\",\"code\":\"BCC\",\"timingPeriod\":{\"start\":\"2019-09-09\",\"end\":\"2019-09-29\"},\"reason\":\"Investigation\",\"goalId\":\"BCC_Focus\",\"subjectCodableConcept\":{\"text\":\"Operational_Area\"},\"taskTemplate\":\"BCC_Focus\"}]}";
 
 	public static String location = "{\n"
 			+ "    \"resourceType\": \"Location\",\n"
@@ -119,6 +122,206 @@ public class QueueHelperTest {
 			+ "    }\n"
 			+ "}";
 
+	public static final String plan = "{\n"
+			+ "  \"identifier\": \"test-plan-identifier-\",\n"
+			+ "  \"version\": \"1\",\n"
+			+ "  \"name\": \"FI-Routine-Dynamic-Task-Test-Plan-2020-06-26\",\n"
+			+ "  \"title\": \"FI Routine Dynamic Task-Test Plan 2020-06-26\",\n"
+			+ "  \"status\": \"active\",\n"
+			+ "  \"date\": \"2020-06-26\",\n"
+			+ "  \"effectivePeriod\": {\n"
+			+ "    \"start\": \"2020-06-26\",\n"
+			+ "    \"end\": \"2020-07-13\"\n"
+			+ "  },\n"
+			+ "  \"useContext\": [\n"
+			+ "    {\n"
+			+ "      \"code\": \"fiStatus\",\n"
+			+ "      \"valueCodableConcept\": \"A1\"\n"
+			+ "    },\n"
+			+ "    {\n"
+			+ "      \"code\": \"fiReason\",\n"
+			+ "      \"valueCodableConcept\": \"Routine\"\n"
+			+ "    }\n"
+			+ "  ],\n"
+			+ "  \"jurisdiction\": [\n"
+			+ "    {\n"
+			+ "      \"code\": \"1435f854-e818-45bb-8de1-ee27c5ec4e1c\"\n"
+			+ "    }\n"
+			+ "  ],\n"
+			+ "  \"goal\": [\n"
+			+ "    {\n"
+			+ "      \"id\": \"RACD_register_families\",\n"
+			+ "      \"description\": \"Register all families &amp; family members in all residential structures enumerated (100%) within the operational area\",\n"
+			+ "      \"priority\": \"medium-priority\",\n"
+			+ "      \"target\": [\n"
+			+ "        {\n"
+			+ "          \"measure\": \"Percent of residential structures with full family registration\",\n"
+			+ "          \"detail\": {\n"
+			+ "            \"detailQuantity\": {\n"
+			+ "              \"value\": 100,\n"
+			+ "              \"comparator\": \">=\",\n"
+			+ "              \"unit\": \"Percent\"\n"
+			+ "            }\n"
+			+ "          },\n"
+			+ "          \"due\": \"2020-07-06\"\n"
+			+ "        }\n"
+			+ "      ]\n"
+			+ "    },\n"
+			+ "    {\n"
+			+ "      \"id\": \"RACD_Blood_Screening\",\n"
+			+ "      \"description\": \"Visit all residential structures (100%) within a 1 km radius of a confirmed index case and test each registered person\",\n"
+			+ "      \"priority\": \"medium-priority\",\n"
+			+ "      \"target\": [\n"
+			+ "        {\n"
+			+ "          \"measure\": \"Number of registered people tested\",\n"
+			+ "          \"detail\": {\n"
+			+ "            \"detailQuantity\": {\n"
+			+ "              \"value\": 100,\n"
+			+ "              \"comparator\": \">=\",\n"
+			+ "              \"unit\": \"Person(s)\"\n"
+			+ "            }\n"
+			+ "          },\n"
+			+ "          \"due\": \"2020-07-06\"\n"
+			+ "        }\n"
+			+ "      ]\n"
+			+ "    },\n"
+			+ "    {\n"
+			+ "      \"id\": \"RACD_bednet_distribution\",\n"
+			+ "      \"description\": \"Visit 100% of residential structures in the operational area and provide nets\",\n"
+			+ "      \"priority\": \"medium-priority\",\n"
+			+ "      \"target\": [\n"
+			+ "        {\n"
+			+ "          \"measure\": \"Percent of residential structures received nets\",\n"
+			+ "          \"detail\": {\n"
+			+ "            \"detailQuantity\": {\n"
+			+ "              \"value\": 100,\n"
+			+ "              \"comparator\": \">=\",\n"
+			+ "              \"unit\": \"Percent\"\n"
+			+ "            }\n"
+			+ "          },\n"
+			+ "          \"due\": \"2020-07-06\"\n"
+			+ "        }\n"
+			+ "      ]\n"
+			+ "    },\n"
+			+ "    {\n"
+			+ "      \"id\": \"Larval_Dipping\",\n"
+			+ "      \"description\": \"Perform a minimum of three larval dipping activities in the operational area\",\n"
+			+ "      \"priority\": \"medium-priority\",\n"
+			+ "      \"target\": [\n"
+			+ "        {\n"
+			+ "          \"measure\": \"Number of larval dipping activities completed\",\n"
+			+ "          \"detail\": {\n"
+			+ "            \"detailQuantity\": {\n"
+			+ "              \"value\": 3,\n"
+			+ "              \"comparator\": \">=\",\n"
+			+ "              \"unit\": \"activit(y|ies)\"\n"
+			+ "            }\n"
+			+ "          },\n"
+			+ "          \"due\": \"2020-07-06\"\n"
+			+ "        }\n"
+			+ "      ]\n"
+			+ "    },\n"
+			+ "    {\n"
+			+ "      \"id\": \"Mosquito_Collection\",\n"
+			+ "      \"description\": \"Set a minimum of three mosquito collection traps and complete the mosquito collection process\",\n"
+			+ "      \"priority\": \"medium-priority\",\n"
+			+ "      \"target\": [\n"
+			+ "        {\n"
+			+ "          \"measure\": \"Number of mosquito collection activities completed\",\n"
+			+ "          \"detail\": {\n"
+			+ "            \"detailQuantity\": {\n"
+			+ "              \"value\": 3,\n"
+			+ "              \"comparator\": \">=\",\n"
+			+ "              \"unit\": \"activit(y|ies)\"\n"
+			+ "            }\n"
+			+ "          },\n"
+			+ "          \"due\": \"2020-07-06\"\n"
+			+ "        }\n"
+			+ "      ]\n"
+			+ "    },\n"
+			+ "    {\n"
+			+ "      \"id\": \"BCC_Focus\",\n"
+			+ "      \"description\": \"Complete at least 1 BCC activity for the operational area\",\n"
+			+ "      \"priority\": \"medium-priority\",\n"
+			+ "      \"target\": [\n"
+			+ "        {\n"
+			+ "          \"measure\": \"BCC Activities Completed\",\n"
+			+ "          \"detail\": {\n"
+			+ "            \"detailQuantity\": {\n"
+			+ "              \"value\": 1,\n"
+			+ "              \"comparator\": \">=\",\n"
+			+ "              \"unit\": \"activit(y|ies)\"\n"
+			+ "            }\n"
+			+ "          },\n"
+			+ "          \"due\": \"2020-07-06\"\n"
+			+ "        }\n"
+			+ "      ]\n"
+			+ "    }\n"
+			+ "  ],\n"
+			+ "  \"action\": [\n"
+			+ "    {\n"
+			+ "      \"identifier\": \"d1c6dac7-1ce8-4723-b28d-ac20621f171e\",\n"
+			+ "      \"prefix\": 1,\n"
+			+ "      \"title\": \"Family Registration\",\n"
+			+ "      \"description\": \"Register all families &amp; family members in all residential structures enumerated (100%) within the operational area\",\n"
+			+ "      \"code\": \"RACD Register Family\",\n"
+			+ "      \"trigger\": [\n"
+			+ "        {\n"
+			+ "          \"type\": \"named-event\",\n"
+			+ "          \"name\": \"plan-activation\"\n"
+			+ "        },\n"
+			+ "        {\n"
+			+ "          \"type\": \"named-event\",\n"
+			+ "          \"name\": \"event-submission\",\n"
+			+ "          \"expression\": {\n"
+			+ "            \"expression\": \"questionnaire = 'Register_Structure'\"\n"
+			+ "          }\n"
+			+ "        }\n"
+			+ "      ],\n"
+			+ "      \"condition\": [\n"
+			+ "        {\n"
+			+ "          \"kind\": \"applicability\",\n"
+			+ "          \"expression\": {\n"
+			+ "            \"description\": \"Structure is residential\",\n"
+			+ "            \"reference\": \"plan-activation\",\n"
+			+ "            \"expression\": \"$this.type.where(id='locationType').text = 'Residential Structure'\"\n"
+			+ "          }\n"
+			+ "        },\n"
+			+ "        {\n"
+			+ "          \"kind\": \"applicability\",\n"
+			+ "          \"expression\": {\n"
+			+ "            \"description\": \"Family does not exist for structure\",\n"
+			+ "            \"reference\": \"plan-activation\",\n"
+			+ "            \"expression\": \"$this.contained.exists().not()\",\n"
+			+ "            \"subjectCodableConcept\": {\n"
+			+ "              \"text\": \"Family\"\n"
+			+ "            }\n"
+			+ "          }\n"
+			+ "        },\n"
+			+ "        {\n"
+			+ "          \"kind\": \"applicability\",\n"
+			+ "          \"expression\": {\n"
+			+ "            \"description\": \"Register stucture Event submitted for a residential structure\",\n"
+			+ "            \"reference\": \"event-submission\",\n"
+			+ "            \"expression\": \"questionnaire = 'Register_Structure' AND item.where(linkId='structureType').answer.value ='Residential Structure'\"\n"
+			+ "          }\n"
+			+ "        }\n"
+			+ "      ],\n"
+			+ "      \"timingPeriod\": {\n"
+			+ "        \"start\": \"2020-06-26\",\n"
+			+ "        \"end\": \"2020-07-06\"\n"
+			+ "      },\n"
+			+ "      \"reason\": \"Routine\",\n"
+			+ "      \"goalId\": \"RACD_register_families\",\n"
+			+ "      \"subjectCodableConcept\": {\n"
+			+ "        \"text\": \"Location\"\n"
+			+ "      },\n"
+			+ "      \"definitionUri\": \"thailand_family_register.json\"\n"
+			+ "    }\n"
+			+ "  ],\n"
+			+ "  \"experimental\": false\n"
+			+ "}";
+
 	@Before
 	public void setup() {
 		initMocks(this);
@@ -138,10 +341,11 @@ public class QueueHelperTest {
 	public void testAddToQueue() {
 		PlanDefinition planDefinition = createPlan();
 		when(planService.getPlan(anyString())).thenReturn(planDefinition);
+		addNewPlanToRepo();
 		Mockito.doNothing().when(planEvaluator)
 				.evaluatePlan(any(PlanDefinition.class), any(TriggerType.class), any(Jurisdiction.class), any(
 						QuestionnaireResponse.class));
-		queueHelper.addToQueue("planid", TriggerType.PLAN_ACTIVATION, "loc-1");
+		queueHelper.addToQueue("test-plan-identifier-", TriggerType.PLAN_ACTIVATION, "loc-1");
 		int count = (Integer) amqpAdmin.getQueueProperties(queue.getName()).get("QUEUE_MESSAGE_COUNT");
 		assertEquals(0, count); // This shows message has been consumed
 	}
@@ -183,6 +387,16 @@ public class QueueHelperTest {
 						eq(TriggerType.PLAN_ACTIVATION));
 	}
 
+	@Test
+	public void AddToQueueV2WithQueuingDisabledAndInvalidResource() {
+		Whitebox.setInternalState(queueHelper, "isQueuingEnabled", false);
+		Action action = createAction();
+		queueHelper.addToQueue(plan, null, action, "plan-id", "jur-id", TriggerType.PLAN_ACTIVATION);
+		verify(planEvaluator, never())
+				.evaluateResource(eq(domainResourceArgumentCaptor.capture()), null, eq(action), eq("plan-id"), eq("jur-id"),
+						eq(TriggerType.PLAN_ACTIVATION));
+	}
+
 	public static PlanDefinition createPlan() {
 		return gson.fromJson(plan, PlanDefinition.class);
 	}
@@ -193,6 +407,11 @@ public class QueueHelperTest {
 
 	public static Location createLocation() {
 		return Location.builder().id(UUID.randomUUID().toString()).name(of("Nairobi")).status(LocationStatus.ACTIVE).build();
+	}
+
+	public void addNewPlanToRepo() {
+		PlanDefinition planDefinition = createPlan();
+		planRepository.add(planDefinition);
 	}
 
 	public static Action createAction() {
