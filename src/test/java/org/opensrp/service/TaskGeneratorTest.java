531f08d26fb7424cc2359ff8dd8050ec1e474569
richard <rkareko@ona.io>
2020-09-17 13:19:41 +0300

    Added tests

---
+++ src/test/java/org/opensrp/service/TaskGeneratorTest.java
@@ -0,0 +1,69 @@
+package org.opensrp.service;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.smartregister.domain.ExecutionPeriod;
+import org.smartregister.domain.PlanDefinition;
+
+import java.util.Collections;
+
+public class TaskGeneratorTest {
+
+	@Rule
+	public MockitoRule rule = MockitoJUnit.rule();
+
+	TaskGenerator taskGenerator;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		taskGenerator = new TaskGenerator();
+	}
+
+	@Test
+	public void testIsInternalTaskGenerationPlanWithInternalTaskGenerationStatusReturnsTrue() {
+
+		PlanDefinition plan = new PlanDefinition();
+		plan.setIdentifier("plan-id-1");
+		plan.setStatus(PlanDefinition.PlanStatus.ACTIVE);
+		ExecutionPeriod executionPeriod = new ExecutionPeriod();
+		executionPeriod.setEnd(new LocalDate().plusYears(2));
+		plan.setEffectivePeriod(executionPeriod);
+		PlanDefinition.UseContext useContext = new PlanDefinition.UseContext();
+		useContext.setCode("taskGenerationStatus");
+		useContext.setValueCodableConcept("internal");
+		plan.setUseContext(Collections.singletonList(useContext));
+
+		boolean isInternalTaskGeneration = taskGenerator.isInternalTaskGeneration(plan);
+
+		Assert.assertTrue(isInternalTaskGeneration);
+
+	}
+
+	@Test
+	public void testIsInternalTaskGenerationPlanWithoutInternalTaskGenerationStatusReturnsTrue() {
+
+		PlanDefinition plan = new PlanDefinition();
+		plan.setIdentifier("plan-id-1");
+		plan.setStatus(PlanDefinition.PlanStatus.ACTIVE);
+		ExecutionPeriod executionPeriod = new ExecutionPeriod();
+		executionPeriod.setEnd(new LocalDate().plusYears(2));
+		plan.setEffectivePeriod(executionPeriod);
+		PlanDefinition.UseContext useContext = new PlanDefinition.UseContext();
+		useContext.setCode("taskGenerationStatus");
+		useContext.setValueCodableConcept("True");
+		plan.setUseContext(Collections.singletonList(useContext));
+
+		boolean isInternalTaskGeneration = taskGenerator.isInternalTaskGeneration(plan);
+
+		Assert.assertFalse(isInternalTaskGeneration);
+
+	}
+
+}
