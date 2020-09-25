d70765aa331716f461c1505f1ca65da4c30f89f4
Samuel Githengi <samuel.githengi@gmail.com>
2020-09-25 10:44:10 +0300

    Add dateRange and daterange type handler

---
+++ src/main/java/org/opensrp/domain/postgres/DateRange.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.opensrp.domain.postgres;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Samuel Githengi created on 09/25/20
+ */
+@Data
+@AllArgsConstructor
+public class DateRange {
+	
+	private Date fromdate;
+	
+	private Date toDate;
+}
