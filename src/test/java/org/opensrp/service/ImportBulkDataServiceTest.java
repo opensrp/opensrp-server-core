5e444c409d685c3f84d4fe090872ad2081caac85
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-09-25 19:17:38 +0500

    Fix codacy issue

--- src/test/java/org/opensrp/service/ImportBulkDataServiceTest.java
+++ src/test/java/org/opensrp/service/ImportBulkDataServiceTest.java
@@ -17,7 +17,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
