251f2c8dcd20139bc8aa3078694c271c77f18752
Rodgers Andati <andatirodgers@gmail.com>
2020-10-05 15:18:28 +0300

    bump up version

--- src/main/java/org/opensrp/service/SafetyNetService.java
+++ src/main/java/org/opensrp/service/SafetyNetService.java
@@ -1,6 +1,10 @@
 package org.opensrp.service;
 
-import com.google.api.client.http.*;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
