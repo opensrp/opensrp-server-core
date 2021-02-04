2e5b770b9bfdc1c81f5b598b8e63644496a9d408
Samuel Githengi <samuel.githengi@gmail.com>
2021-02-04 19:21:59 +0300

    Implement get tasks by jurisdiction

--- src/main/java/org/opensrp/repository/postgres/TaskRepositoryImpl.java
+++ src/main/java/org/opensrp/repository/postgres/TaskRepositoryImpl.java
@@ -422,8 +422,10 @@ public class TaskRepositoryImpl extends BaseRepositoryImpl<Task> implements Task
 	}
 
 	@Override
-	public List<com.ibm.fhir.model.resource.Task> findTasksByJurisdiction(String s) {
-		throw new NotImplementedException();
+	public List<com.ibm.fhir.model.resource.Task> findTasksByJurisdiction(String jurisdiction, String planIdentifier) {
+		TaskMetadataExample example = new TaskMetadataExample();
+		example.createCriteria().andPlanIdentifierEqualTo(planIdentifier).andGroupIdentifierEqualTo(jurisdiction);
+		return convertToFHIRTasks(convert(taskMetadataMapper.selectMany(example, 0, DEFAULT_FETCH_SIZE)));
 	}
 
 	@Override
