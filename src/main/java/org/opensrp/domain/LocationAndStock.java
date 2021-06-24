b52f7e210ac6f53035fc083b42541b6bc03a287d
Benn Simon <botieno@ona.io>
2021-06-24 12:30:49 +0300

    Add location stock domain with join query (#473)
    * add logic for LocationAndStock domain with relevant join queries
    
    * add stockMetadataExample where clause
    
    * code cleanup
    
    * update snapshot version
    
    * update code to use PhysicalLocationAndStocks domain from plan evaluator
    
    * update limit logic
    
    * update plan evaluator lib
    
    * add v2 to github actions branch
    
    * code cleanup
    
    * update method doc

---
+++ src/main/java/org/opensrp/domain/LocationAndStock.java
@@ -0,0 +1,20 @@
+package org.opensrp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.opensrp.domain.postgres.Location;
+import org.opensrp.domain.postgres.Stock;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LocationAndStock extends Location {
+
+	private List<Stock> stocks;
+
+}
