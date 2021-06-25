c584e5ca47b2485b918f6bafb17264b5192b09ad
richard <rkareko@ona.io>
2021-06-25 07:55:02 +0300

    Code cleanup

--- src/test/java/org/opensrp/service/UploadServiceTest.java
+++ src/test/java/org/opensrp/service/UploadServiceTest.java
@@ -23,8 +23,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 
 
