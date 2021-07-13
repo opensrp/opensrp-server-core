a19c10a6ec4b0f669994a78a0d1a8ab0b0da6551
Elly Kitoto <junkmailstoelly@gmail.com>
2021-07-13 23:31:57 +0300

    Add missing residential_address attribute to child client

--- src/main/java/org/opensrp/domain/rapidpro/converter/zeir/ZeirChildClientConverter.java
+++ src/main/java/org/opensrp/domain/rapidpro/converter/zeir/ZeirChildClientConverter.java
@@ -1,6 +1,5 @@
 package org.opensrp.domain.rapidpro.converter.zeir;
 
-import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.opensrp.domain.rapidpro.contact.zeir.RapidProContact;
 import org.opensrp.domain.rapidpro.contact.zeir.RapidProFields;
@@ -19,33 +18,31 @@ public class ZeirChildClientConverter extends BaseRapidProClientConverter {
 
 		RapidProFields fields = rapidProContact.getFields();
 		if (RapidProConstants.CHILD.equalsIgnoreCase(fields.getPosition())) {
-			Client client = new Client(rapidProContact.getUuid());
-			setCommonZeirClientAttributes(rapidProContact, client);
+			Client childClient = new Client(rapidProContact.getUuid());
+			setCommonZeirClientAttributes(rapidProContact, childClient);
 
-			String zeirId = getIdentifier(rapidProContact);
-			if (StringUtils.isNoneBlank(zeirId)) {
-				client.getIdentifiers().put(RapidProConstants.ZEIR_ID, zeirId.replaceAll("-", ""));
-			}
+			addZeirClientIdentifier(rapidProContact, childClient, RapidProConstants.ZEIR_ID);
 
-			client.setBirthdate(new DateTime(Instant.parse(fields.getDob()).toEpochMilli()));
-			client.addAttribute(RapidProConstants.CHILD_REGISTER_CARD_NUMBER, fields.getMvaccId());
-			client.addAttribute(RapidProConstants.RESIDENTIAL_AREA, fields.getLocation());
+			childClient.setBirthdate(new DateTime(Instant.parse(fields.getDob()).toEpochMilli()));
+			childClient.addAttribute(RapidProConstants.CHILD_REGISTER_CARD_NUMBER, fields.getMvaccId());
+			childClient.addAttribute(RapidProConstants.RESIDENTIAL_AREA, fields.getLocation());
+			childClient.addAttribute(RapidProConstants.RESIDENTIAL_ADDRESS, fields.getLocation());
 
 			//Place of birth facility saved as health_facility in ZEIR
 			String birth = RapidProConstants.FACILITY.equalsIgnoreCase(fields.getBirth()) ?
 					RapidProConstants.HEALTH_FACILITY :
 					fields.getBirth();
-			client.addAttribute(RapidProConstants.PLACE_OF_BIRTH, birth.toLowerCase(Locale.ROOT).replaceAll(" ", "_"));
+			childClient.addAttribute(RapidProConstants.PLACE_OF_BIRTH, birth.toLowerCase(Locale.ROOT).replaceAll(" ", "_"));
 
-			//Use Supervisor's location as the default place of birth which is saved as location id address in client json object
+			//Use Supervisor's location as the default place of birth which is saved as location id address in childClient json object
 			Address address = new Address();
 			address.setAddressType(RapidProConstants.USUAL_RESIDENCE);
 			address.addAddressField(RapidProConstants.ADDRESS_SIX, fields.getFacilityLocationId());
-			client.addAddress(address);
+			childClient.addAddress(address);
 
-			client.setGender(fields.getSex().toLowerCase(Locale.ROOT));
+			childClient.setGender(fields.getSex().toLowerCase(Locale.ROOT));
 
-			return client;
+			return childClient;
 		}
 		return null;
 	}
