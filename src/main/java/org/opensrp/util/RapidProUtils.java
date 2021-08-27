ff4b9dbec3cfa6f73910e8683160ba4d0a174b9d
Elly Kitoto <junkmailstoelly@gmail.com>
2021-08-27 19:44:06 +0300

    Change log format

--- src/main/java/org/opensrp/util/RapidProUtils.java
+++ src/main/java/org/opensrp/util/RapidProUtils.java
@@ -68,24 +68,24 @@ public class RapidProUtils {
 				case HttpStatus.SC_OK:
 				case HttpStatus.SC_CREATED:
 				case HttpStatus.SC_NO_CONTENT:
-					logger.info("[Status {}] RapidPro resource successfully listed/created/updated",
+					logger.info("Status {}: RapidPro resource successfully listed/created/updated",
 							statusLine.getStatusCode());
 					break;
 				case HttpStatus.SC_BAD_REQUEST:
-					logger.error("[Status {}] RapidPro request failed due to invalid parameters",
+					logger.error("Status {}: RapidPro request failed due to invalid parameters",
 							statusLine.getStatusCode());
 					logger.error(EntityUtils.toString(httpResponse.getEntity()));
 					break;
 				case HttpStatus.SC_FORBIDDEN:
-					logger.warn("[Status {}] RapidPro request failed due to lack of permissions to access resource",
+					logger.warn("Status {}: RapidPro request failed due to lack of permissions to access resource",
 							statusLine.getStatusCode());
 					logger.warn(EntityUtils.toString(httpResponse.getEntity()));
 					break;
 				case HttpStatus.SC_NOT_FOUND:
-					logger.warn("[Status {}] RapidPro Resource was not found", statusLine.getStatusCode());
+					logger.warn("Status {}: RapidPro Resource was not found", statusLine.getStatusCode());
 					break;
 				case RATE_LIMIT_EXCEEDED:
-					logger.warn("[Status {}] RapidPro rate limit for the endpoint has been exceeded",
+					logger.warn("Status {}: RapidPro rate limit for the endpoint has been exceeded",
 							statusLine.getStatusCode());
 					logger.warn(EntityUtils.toString(httpResponse.getEntity()));
 					break;
