44329d8135e2e8f57b569726bcb327f134c7c873
Richard Kareko <rkareko@ona.io>
2021-12-06 07:17:43 +0300

    Add missing semi colon

--- src/test/java/org/opensrp/service/PlanServiceTest.java
+++ src/test/java/org/opensrp/service/PlanServiceTest.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map
+import java.util.Map;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Before;
