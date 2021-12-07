92c681d55aebf5e6b4950a6fd0f431d82c218da5
Richard Kareko <rkareko@ona.io>
2021-12-07 15:20:43 +0300

    Add unit plan processing status unit tests

--- src/test/java/org/opensrp/repository/postgres/PlanProcessingStatusRepositoryTest.java
+++ src/test/java/org/opensrp/repository/postgres/PlanProcessingStatusRepositoryTest.java
@@ -82,19 +82,67 @@ public class PlanProcessingStatusRepositoryTest extends BaseRepositoryTest{
     @Test
     public void testGetByStatusReturnsCorrectValue() {
 
-        PlanProcessingStatus status = planProcessingStatusRepository.getByEventId(1l);
+        List<PlanProcessingStatus> statusList = planProcessingStatusRepository.getByStatus(PlanProcessingStatusConstants.INITIAL);
+        assertEquals(PlanProcessingStatusConstants.INITIAL, statusList.get(0).getStatus().intValue());
+        assertEquals(1l, statusList.get(0).getPlanId().longValue());
+        assertEquals(1l, statusList.get(0).getTemplateId().longValue());
+        assertEquals(1l, statusList.get(0).getEventId().longValue());
+    }
+
+    @Test
+    public void testAddPlanProcessingStatus() {
+        List<PlanProcessingStatus> statusList = planProcessingStatusRepository.getByStatus(PlanProcessingStatusConstants.COMPLETE);
+        assertTrue(statusList.isEmpty());
+        planProcessingStatusRepository.addPlanProcessingStatus("1c3f6eac-a765-4423-bbda-c01ed6430ae8",PlanProcessingStatusConstants.COMPLETE);
+
+        List<PlanProcessingStatus> addedStatusList = planProcessingStatusRepository.getByStatus(PlanProcessingStatusConstants.COMPLETE);
+        assertEquals(1, addedStatusList.size());
+        assertEquals(PlanProcessingStatusConstants.COMPLETE, addedStatusList.get(0).getStatus().intValue());
+        assertEquals(2l, addedStatusList.get(0).getEventId().longValue());
+    }
+
+    @Test
+    public void testUpdatePlanProcessingStatus() {
+
+        PlanProcessingStatus status = planProcessingStatusRepository.getByPrimaryKey(1l);
         assertEquals(PlanProcessingStatusConstants.INITIAL, status.getStatus().intValue());
         assertEquals(1l, status.getPlanId().longValue());
         assertEquals(1l, status.getTemplateId().longValue());
         assertEquals(1l, status.getEventId().longValue());
+
+        planProcessingStatusRepository.updatePlanProcessingStatus(status, "1c3f6eac-a765-4423-bbda-c01ed6430ae8",
+                "3450034c-1ba5-556d-8b16-71266397b8b9", PlanProcessingStatusConstants.PROCESSING);
+
+        PlanProcessingStatus updatedStatus = planProcessingStatusRepository.getByPrimaryKey(1l);
+        assertEquals(PlanProcessingStatusConstants.PROCESSING, updatedStatus.getStatus().intValue());
+        assertEquals(2l, updatedStatus.getPlanId().longValue());
+        assertEquals(1l, updatedStatus.getTemplateId().longValue());
+        assertEquals(2l, updatedStatus.getEventId().longValue());
+    }
+
+    @Test
+    public void testAddShouldCreateNewPlanProcessingStatus() {
+
+        List<PlanProcessingStatus> statusList = planProcessingStatusRepository.getByStatus(PlanProcessingStatusConstants.COMPLETE);
+        assertTrue(statusList.isEmpty());
+        PlanProcessingStatus status = initTestProcessingStatus();
+        planProcessingStatusRepository.add(status);
+
+        List<PlanProcessingStatus> addedStatusList = planProcessingStatusRepository.getByStatus(PlanProcessingStatusConstants.COMPLETE);
+        assertEquals(1, addedStatusList.size());
+        assertEquals(PlanProcessingStatusConstants.COMPLETE, addedStatusList.get(0).getStatus().intValue());
+        assertEquals(2l, addedStatusList.get(0).getEventId().longValue());
+        assertEquals(2l, addedStatusList.get(0).getPlanId().longValue());
+        assertEquals(1l, addedStatusList.get(0).getTemplateId().longValue());
+
     }
 
     private PlanProcessingStatus initTestProcessingStatus() {
         PlanProcessingStatus planProcessingStatus = new PlanProcessingStatus();
         planProcessingStatus.setStatus(PlanProcessingStatusConstants.COMPLETE);
-        planProcessingStatus.setPlanId(1l);
+        planProcessingStatus.setPlanId(2l);
         planProcessingStatus.setTemplateId(1l);
-        planProcessingStatus.setEventId(1l);
+        planProcessingStatus.setEventId(2l);
         return planProcessingStatus;
     }
 }
