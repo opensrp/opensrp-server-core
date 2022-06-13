a824d019bfb2998c7d02d0244666ed9e51bea9bc
bennsimon <botieno@ona.io>
2022-06-13 12:13:54 +0300

    code cleanup

--- src/test/java/org/opensrp/TestPostgresInstance.java
+++ src/test/java/org/opensrp/TestPostgresInstance.java
@@ -7,15 +7,20 @@ public abstract class TestPostgresInstance {
 
 	private static final String POSTGRES_USER = "opensrp_admin";
 	private static final String POSTGRES_PASSWORD = "admin";
+	private static final String POSTGRES_USER_CONFIG = "POSTGRES_USER";
+
+	private static final String POSTGRES_PASSWORD_CONFIG = "POSTGRES_PASSWORD";
+
+	protected static final String POSTGRES_DB_CONFIG = "POSTGRES_DB";
 	protected static final String POSTGRES_DB = "opensrp_test";
 	private static final String DOCKER_IMAGE_NAME = "postgis/postgis:12-2.5-alpine";
 
 	protected static final int DOCKER_EXPOSE_PORT = 5432;
 
 	public static final GenericContainer<?> postgresContainer = new GenericContainer<>(DockerImageName.parse(DOCKER_IMAGE_NAME))
-			.withEnv("POSTGRES_PASSWORD", POSTGRES_PASSWORD)
-			.withEnv("POSTGRES_USER", POSTGRES_USER)
-			.withEnv("POSTGRES_DB", POSTGRES_DB)
+			.withEnv(POSTGRES_PASSWORD_CONFIG, POSTGRES_PASSWORD)
+			.withEnv(POSTGRES_USER_CONFIG, POSTGRES_USER)
+			.withEnv(POSTGRES_DB_CONFIG, POSTGRES_DB)
 		.withExposedPorts(DOCKER_EXPOSE_PORT);
 
 	static {
--- src/test/java/org/opensrp/TestRabbitMQInstance.java
+++ src/test/java/org/opensrp/TestRabbitMQInstance.java
@@ -8,10 +8,13 @@ import org.testcontainers.utility.DockerImageName;
 public abstract class TestRabbitMQInstance {
 	private static final String DOCKER_IMAGE_NAME = "rabbitmq:3-alpine";
 
-	private static final String RABBITMQ_USERNAME = "rabbitmq.username";
+	private static final String RABBITMQ_USERNAME_CONFIG = "rabbitmq.username";
 
-	private static final String RABBITMQ_PASSWORD = "rabbitmq.password";
+	private static final String RABBITMQ_PASSWORD_CONFIG = "rabbitmq.password";
 
+	private static final String RABBITMQ_USERNAME = "guest";
+
+	private static final String RABBITMQ_PASSWORD = "guest";
 	private static final String RABBITMQ_PORT = "rabbitmq.port";
 
 	private static final int DOCKER_EXPOSE_PORT = 5672;
@@ -26,7 +29,7 @@ public abstract class TestRabbitMQInstance {
 	@DynamicPropertySource
 	private static void registerRabbitMQProperties(DynamicPropertyRegistry registry) {
 		registry.add(RABBITMQ_PORT, () -> rabbitMQContainer.getMappedPort(DOCKER_EXPOSE_PORT));
-		registry.add(RABBITMQ_USERNAME, () -> "guest");
-		registry.add(RABBITMQ_PASSWORD, () -> "guest");
+		registry.add(RABBITMQ_USERNAME_CONFIG, () -> RABBITMQ_USERNAME);
+		registry.add(RABBITMQ_PASSWORD_CONFIG, () -> RABBITMQ_PASSWORD);
 	}
 }
