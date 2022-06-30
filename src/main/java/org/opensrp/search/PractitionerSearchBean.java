351aa1f33e24acfb4b6db10f09087602fe856b28
hilpitome <hilpitome@gmail.com>
2022-06-30 11:43:34 +0300

    sort codacy issue

--- src/main/java/org/opensrp/search/PractitionerSearchBean.java
+++ src/main/java/org/opensrp/search/PractitionerSearchBean.java
@@ -2,9 +2,7 @@ package org.opensrp.search;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Data
