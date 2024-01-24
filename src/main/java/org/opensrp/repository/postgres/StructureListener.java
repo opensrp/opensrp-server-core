43ca92dadec32450dcb75f688787ac5ca6498bb0
hilpitome <hilpitome@gmail.com>
2024-01-24 16:54:27 +0300

    more code refactor

--- src/main/java/org/opensrp/repository/postgres/StructureListener.java
+++
@@ -1,7 +0,0 @@
-package org.opensrp.repository.postgres;
-
-import org.opensrp.domain.postgres.Structure;
-
-public interface StructureListener {
-	void onCreateOrUpdateStructure(Structure structure);
-}
